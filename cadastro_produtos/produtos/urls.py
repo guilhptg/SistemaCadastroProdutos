846891745eb7a2ccaccbf52fc83717beaf55153d
Guilherme Portugal <flordavidaho@gmail.com>
2025-09-24 03:10:31 -0300

    Update urls.py

--- cadastro_produtos/produtos/urls.py
+++ cadastro_produtos/produtos/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import *
-from django.contrib.auth import views
+from .views  import *
 
 urlpatterns = [
-    path('', cadastro_produtos, name='cadastro_produtos'),
+    path("", cadastro_produtos, name="cadastro_produtos"),
+    path("editar/<int:pk>/", editar_produto, name="editar_produto"),
+    path("excluir/<int:pk>/", excluir_produto, name="excluir_produto"),
 ]
