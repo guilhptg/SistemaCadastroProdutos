b251d2b8c39ebd3bc3f4f3e03292ff644dee5bf6
Guilherme Portugal <flordavidaho@gmail.com>
2025-09-24 00:15:34 -0300

    Update models.py

--- cadastro_produtos/produtos/models.py
+++ cadastro_produtos/produtos/models.py
@@ -1,3 +1,45 @@
 from django.db import models
 
 # Create your models here.
+class Categoria(models.Model):
+    nome = models.CharField(max_length=50, unique=True)
+
+    class Meta:
+        ordering = ['nome']
+        verbose_name = "Categoria"
+        verbose_name_plural = "Categorias"
+        
+    def __str__(self):
+        return self.nome
+
+
+class Produto(models.Model):
+    nome = models.CharField(max_length=100)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE, related_name="produtos")
+    preco_venda = models.DecimalField(max_digits=10, decimal_places=2)
+    unidade_medida = models.CharField(
+        max_length=20,
+        choices=[
+            ("UN", "Unidade"),
+            ("ML", "Mililitro"),
+            ("L", "Litro"),
+            ("CX", "Caixa"),
+        ],
+        default="UN"
+    )
+    
+    estoque_atual = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    estoque_minimo = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    estoque_maximo = models.DecimalField(max_digits=10, decimal_places=2, default=100)
+    data_cadastro = models.DateTimeField(auto_now_add=True)
+    data_atualizacao = models.DateTimeField(auto_now=True)
+    ativo = models.BooleanField(default=True)
+    
+    class Meta:
+        unique_together = ('nome', 'categoria')
+        ordering = ['name'] 
+        verbose_name = "Produto"
+        verbose_name_plural = "Produtos"
+        
+    def __str__(self):
+        return f"{self.nome} - {self.categoria.nome}"
